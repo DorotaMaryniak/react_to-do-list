672fb82d4fa4f1745ba86aa65c101c29c84d5703
Dorota Maryniak <docia209aa@gmail.com>
2023-10-25 20:10:16 +0200

    Add new column and add new card functions

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -2,13 +2,15 @@
 import { useState } from 'react';
 import Button from '../Button/Button';
 import TextInput from '../TextInput/TextInput';
-import styles from './CardForm.module.scss'
+import styles from './CardForm.module.scss';
+import { useDispatch } from 'react-redux';
 
 const CardForm =  props => {
 const [title, setTitle] = useState('');
+const dispatch = useDispatch();
 const handleSubmit = e => {
     e.preventDefault();
-    props.action({ title: title }, props.columnId);
+    dispatch({ type: 'ADD_CARD', payload:{ title, columnId:props.columnId} });
      //wywolanie funkcji otrzymanej od parametru action w List, przekazuje jej inf co było w input value
     setTitle('');
     }
