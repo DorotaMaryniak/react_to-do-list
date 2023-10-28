74ccbce65373004bf7d5cbff104b50399f23c316
Dorota Maryniak <docia209aa@gmail.com>
2023-10-28 16:48:26 +0200

    Change NavBar add new list to initialState

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,15 +1,18 @@
 import styles from './NavBar.module.scss';
+import { Link } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
+
 const NavBar = () => {
 return(
-    <nav className={styles.nav}>
-        <a href="/"><i className = "fa fa-tasks"></i></a>
+<nav className={styles.nav}>
+    <a href="/"><i className = "fa fa-tasks"></i></a>
 
     <ul>
-         <a href="/">Home</a>
-         <a href="/favorite">Favorite</a>
-         <a href="/about">About</a>
+        <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/">Home</NavLink></li>
+        <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/favorite">Favorite</NavLink></li>
+        <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/about">About</NavLink></li>
     </ul>
-    </nav>
+</nav>
 );
 }
 
