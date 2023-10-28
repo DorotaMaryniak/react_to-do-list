4ce2b0c1cd8fadfc647f8b3ce068327d86d47630
Dorota Maryniak <docia209aa@gmail.com>
2023-10-28 23:30:15 +0200

    Change SearchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,23 +1,23 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useState } from 'react';
-import { updateSearchString } from '../../redux/store';
+import { updateSearchString, getSearchString } from '../../redux/store';
 
 const SearchForm = () => {
-    let string ='';
-
     const dispatch = useDispatch();
+    const searchString = useSelector(getSearchString);
+    const[searchValue, setSearchValue] = useState(searchString);
     const handleSubmit = e =>{
         e.preventDefault();
         console.log('eventtarget',e.target);
-        dispatch(updateSearchString(string));
+        dispatch(updateSearchString(searchValue));
             }
     return (
 
         <form onSubmit={handleSubmit} className={styles.searchForm}>
-            <TextInput placeholder = "Search..."  onChange={e => string=e.target.value} />
+            <TextInput placeholder = "Search..." value = {searchValue} onChange={e => setSearchValue(e.target.value)} />
             <Button>
              <span className="fa fa-search" />
             </Button>
--- src/redux/store.js
+++ src/redux/store.js
@@ -5,6 +5,7 @@ import strContains from '../utils/strContains';
 export const getFilteredCards = ({cards,searchString}, columnId) => cards
   .filter(card => card.columnId === columnId && strContains(card.title,searchString));
 export const getAllColumns=(state) =>  state.columns;
+export const getSearchString = (state) => state.searchString;
 export const getListById=({lists}, listId) => lists.find(list=>list.id===listId);
 export const getColumnsByList = ({columns}, listId) => columns.filter(column => column.listId === listId);
 export const getAllLists=(state) =>  state.lists;
