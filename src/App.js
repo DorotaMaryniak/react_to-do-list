2248db5f6e990e01498da5850d681818cb475ee6
Dorota Maryniak <docia209aa@gmail.com>
2023-10-28 16:03:50 +0200

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import Home from './components/Home/Home'
 import Container from './components/Container/Container'
 import NavBar from './components/NavBar/NavBar'
+import About from './components/About/About'
+import Favorite from './components/Favorite/Favorite'
+import { Routes, Route } from 'react-router-dom';
+import NotFound from './components/Notfound/NotFound'
 
 const App = () => {
   return (
   <main>
-    <NavBar/>
+    <NavBar />
     <Container>
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/favorite" element={<Favorite />} />
+        <Route path = "*"  element={<NotFound/>} />
+      </Routes>
     </Container>
   </main>
   );
