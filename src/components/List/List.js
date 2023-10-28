e3e4b8188c3c8553b4e266f65451dc3db7970bc4
Dorota Maryniak <docia209aa@gmail.com>
2023-10-28 21:32:17 +0200

    Add navigate to SearchForm not change

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,11 +1,13 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column';
 import ColumnForm from '../ColumnForm/ColumnForm';
+import SearchForm from '../SearchForm/SearchForm';
 import { useSelector } from 'react-redux';
 import { getAllColumns} from '../../redux/store';
 import { getListById } from '../../redux/store';
 import { getColumnsByList } from '../../redux/store';
 import { useParams } from 'react-router';
+import { Navigate } from 'react-router-dom';
 // action creators
 export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
 
@@ -15,12 +17,14 @@ const List = () => {
    const columns = useSelector(columns => getColumnsByList(columns,listId));
 const listData = useSelector (lists=> getListById(lists,listId));
 
+if (!listData) return <Navigate to = "/"/>
 return(
- <div className={styles.list}>
+  <div className={styles.list}>
     <header className = {styles.header}>
     <h2 className={styles.title}>{listData.title}</h2>
     </header>
     <p className={styles.description}>{listData.description}</p>
+    <SearchForm />
 
     <section className={styles.columns}>
     {columns.map(column => <Column key={column.id} {...column}/>)}
